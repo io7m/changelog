6f82ce2550c17f75cf2125424ad53521248b8adb
Mark Raynsford <code@io7m.com>
2015-08-22 19:19:59 +0000

    Reconfigure projects for 2015 standards and migrate to GitHub.

--- io7m-changelog-documentation/src/main/java/com/io7m/changelog/documentation/CDocumentation.java
+++ io7m-changelog-documentation/src/main/java/com/io7m/changelog/documentation/CDocumentation.java
@@ -1,10 +1,10 @@
 /*
  * Copyright © 2014 <code@io7m.com> http://io7m.com
- * 
+ *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
@@ -19,9 +19,7 @@ package com.io7m.changelog.documentation;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import javax.annotation.Nonnull;
-
-import com.io7m.jaux.UnreachableCodeException;
+import com.io7m.junreachable.UnreachableCodeException;
 
 /**
  * Functions for retrieving the documentation.
@@ -29,7 +27,7 @@ import com.io7m.jaux.UnreachableCodeException;
 
 public final class CDocumentation
 {
-  public static @Nonnull URI getDocumentationXMLLocation()
+  public static URI getDocumentationXMLLocation()
   {
     try {
       return CDocumentation.class.getResource(
