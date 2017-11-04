ffeb51f07c8020f10855222229318a41d3d2e196
Mark Raynsford <code@io7m.com>
2017-11-04 17:19:43 +0000

    New minisite

---
+++ documentation/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"com.io7m.changelog.core"},{"l":"com.io7m.changelog.documentation"},{"l":"com.io7m.changelog.schema"},{"l":"com.io7m.changelog.text"},{"l":"com.io7m.changelog.xom"}]
